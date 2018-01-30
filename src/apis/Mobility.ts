9210b70e252b6b2454a5867d250f02b5b65c168a
Teemu Kurki <teemu.kurki@op.fi>
2018-01-30 15:27:51 +0200

    added query handling

--- src/apis/Mobility.ts
+++ src/apis/Mobility.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+import * as qs from 'querystring';
 import { Options } from '../utils/DataSchemas';
 
 export default class Mobility {
@@ -7,24 +7,55 @@ export default class Mobility {
     constructor(options: Options) {
         this.options = options;
     }
-    async getBranches() {
+    async getBranches(
+        bbody: string = '',
+        location: string = '',
+        query: string = ''
+    ) {
+        const queryString = qs.stringify({
+            bbody: bbody,
+            location: location,
+            query: query
+        });
         const requestOptions = Object.assign({}, this.options, {
             method: 'GET',
-            url: `/mobility/${this.options.version}/branches`
+            url: `/mobility/${this.options.version}/branches?${queryString}`
         });
         return axios(requestOptions);
     }
-    async getBranchesAsJson() {
+    async getBranchesAsJson(
+        bbody: string = '',
+        location: string = '',
+        query: string = ''
+    ) {
+        const queryString = qs.stringify({
+            bbody: bbody,
+            location: location,
+            query: query
+        });
         const requestOptions = Object.assign({}, this.options, {
             method: 'GET',
-            url: `/mobility/${this.options.version}/branches.json`
+            url: `/mobility/${
+                this.options.version
+            }/branches.json?${queryString}`
         });
         return axios(requestOptions);
     }
-    async getBranchesAsGeoJson() {
+    async getBranchesAsGeoJson(
+        bbody: string = '',
+        location: string = '',
+        query: string = ''
+    ) {
+        const queryString = qs.stringify({
+            bbody: bbody,
+            location: location,
+            query: query
+        });
         const requestOptions = Object.assign({}, this.options, {
             method: 'GET',
-            url: `/mobility/${this.options.version}/branches.geojson`
+            url: `/mobility/${
+                this.options.version
+            }/branches.geojson?${queryString}`
         });
         return axios(requestOptions);
     }
